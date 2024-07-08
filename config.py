b17fcd13a4f430e0c8ba87afe379ea668bcd799f
Miko756 <164617880+Miko756@users.noreply.github.com>
2024-07-08 14:25:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002017236954"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "5787502520"))
 
 #Port
-PORT = os.environ.get("PORT", "3059")
+PORT = os.environ.get("PORT", "3060")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Dev_1:Dev_1@swopan.z77tekr.mongodb.net/?retryWrites=true&w=majority&appName=Swopan")
