ca5ac235404a2d48d69795efa6a486c8fabc28c0
Miko756 <164617880+Miko756@users.noreply.github.com>
2024-07-15 16:58:13 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -126,6 +126,14 @@ async def not_joined(client: Client, message: Message):
         [
             InlineKeyboardButton(text="Join Channel", url=client.invitelink),
             InlineKeyboardButton(text="Join Channel", url=client.invitelink2),
+        ],
+        [
+            InlineKeyboardButton(text="Join Channel", url=client.invitelink3),
+            InlineKeyboardButton(text="Join Channel", url=client.invitelink4),
+        ],
+        [
+            InlineKeyboardButton(text="Join Channel", url=client.invitelink5),
+            InlineKeyboardButton(text="Join Channel", url=client.invitelink6),
         ]
     ]
     try:
